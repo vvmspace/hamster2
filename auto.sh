98481d0538166f147dcd9d866ae4f27909068f03
Vladimir V. M. <vvmspace@gmail.com>
2026-02-22 03:45:30 +0400

    just commit

--- auto.sh
+++ auto.sh
@@ -4,7 +4,10 @@ node ./auto.js --url https://miningcombo.com/hamster/ --output ./themes/hamster/
 git add ./themes/hamster/layouts/partials/combo.html
 git add ./themes/hamster/layouts/partials/cipher.html
 git add ./themes/hamster/layouts/partials/morse.html
-# ammend the commit
-git commit --amend --no-edit
-# force push
-git push -f
+# Limit output check to the specific files we added
+if ! git diff --cached --quiet ./themes/hamster/layouts/partials/combo.html ./themes/hamster/layouts/partials/cipher.html ./themes/hamster/layouts/partials/morse.html; then
+  # ammend the commit
+  git commit --amend --no-edit
+  # force push
+  git push -f
+fi
